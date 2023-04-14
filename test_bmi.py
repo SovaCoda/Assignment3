33a2aaaa4bb242e26bf6434092f616f95cc394ee
SovaCoda <80545648+SovaCoda@users.noreply.github.com>
2023-04-13 19:14:35 -0500

    Unittests
    Changed pytests to unittests

--- test_bmi.py
+++ test_bmi.py
@@ -1,22 +1,26 @@
+import unittest
 from bmi import convertWeight, convertHeight, doMath, calculateBMI, classify
-def test_convertWeight():
-    assert convertWeight(100) == 45
 
-def test_convertHeight():
-    assert convertHeight(100) == 2.5
+class TestMyModule(unittest.TestCase):
 
-def test_doMath():
-    assert doMath(45, 2.5) == 7.2
+    def test_convertWeight(self):
+        self.assertEqual(convertWeight(100), 45)
 
-def test_calculateBMI():
-    assert calculateBMI(100, 100) == 7.2
+    def test_convertHeight(self):
+        self.assertEqual(convertHeight(100), 2.5)
 
-def test_classify():
-    assert classify(18.4) == "Underweight"
-    assert classify(18.5) == "Normal Weight"
-    assert classify(20) == "Normal Weight"
-    assert classify(24.9) == "Normal Weight"
-    assert classify(25) == "Overweight"
-    assert classify(26) == "Overweight"
-    assert classify(29.9) == "Overweight"
-    assert classify(30) == "Obese"
+    def test_doMath(self):
+        self.assertEqual(doMath(45, 2.5), 7.2)
+
+    def test_calculateBMI(self):
+        self.assertEqual(calculateBMI(100, 100), 7.2)
+
+    def test_classify(self):
+        self.assertEqual(classify(18.4), "Underweight")
+        self.assertEqual(classify(18.5), "Normal Weight")
+        self.assertEqual(classify(20), "Normal Weight")
+        self.assertEqual(classify(24.9), "Normal Weight")
+        self.assertEqual(classify(25), "Overweight")
+        self.assertEqual(classify(26), "Overweight")
+        self.assertEqual(classify(29.9), "Overweight")
+        self.assertEqual(classify(30), "Obese")
